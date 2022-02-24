b4d59b17d325619d607178bc5da4c4f3fc91c185
CarlosGarcia-97 <20203tn086@utez.edu.mx>
2022-02-23 23:03:15 -0600

    ImagenCapsula y Capsula

--- src/main/java/mx/edu/utez/SIRIACI_servicio/model/capsula/Capsula.java
+++ src/main/java/mx/edu/utez/SIRIACI_servicio/model/capsula/Capsula.java
@@ -1,18 +1,28 @@
 package mx.edu.utez.SIRIACI_servicio.model.capsula;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import mx.edu.utez.SIRIACI_servicio.model.imagenCapsula.ImagenCapsula;
 import mx.edu.utez.SIRIACI_servicio.model.responsable.Responsable;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class Capsula {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(name ="titulo",unique = true, nullable = false)
     private String titulo;
+    @Column(name ="contenido",unique = true, nullable = false)
     private String contenido; //text
+    @Column(name ="activo",unique = true, nullable = false)
     private boolean activo;
+
     @ManyToOne
-    @JoinColumn( name = "responsable_id",nullable = false, unique = true)
+    @JoinColumn( name = "responsable_id", nullable = false, unique = true)
     private Responsable responsable;
+    @OneToMany(mappedBy = "capsula")
+    @JsonIgnore
+    private List<ImagenCapsula> imagenCapsula;
 }
--- src/main/java/mx/edu/utez/SIRIACI_servicio/model/imagenCapsula/ImagenCapsula.java
+++ src/main/java/mx/edu/utez/SIRIACI_servicio/model/imagenCapsula/ImagenCapsula.java
@@ -2,10 +2,17 @@ package mx.edu.utez.SIRIACI_servicio.model.imagenCapsula;
 
 import mx.edu.utez.SIRIACI_servicio.model.capsula.Capsula;
 
+import javax.persistence.*;
 import java.util.Base64;
 
+@Entity
 public class ImagenCapsula {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private Base64 imagen;
+    @Column(nullable = false, columnDefinition = "blob")
+    private byte[] imagen;
+    @ManyToOne
+    @JoinColumn(name = "capsula_id", nullable = false)
     private Capsula capsula;
 }
