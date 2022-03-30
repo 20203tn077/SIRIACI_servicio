ae03f7624c43f8daa082da7d042a1e70f38532da
Rebecca C <20203tn077@utez.edu.mx>
2022-03-30 12:58:55 -0600

    avance estructura para módulo 3

--- src/main/java/mx/edu/utez/SIRIACI_servicio/controller/Capsulas/CapsulaService.java
+++ src/main/java/mx/edu/utez/SIRIACI_servicio/controller/Capsulas/CapsulaService.java
@@ -6,13 +6,51 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 public class CapsulaService {
-//    @Transactional(rollbackFor = {SQLException.class})
-//    public ResponseEntity<Mensaje> entrada() {
+    //3.1 Consultar cápsulas informativas
+//    @Transactional(readOnly = true)
+//    public ResponseEntity<Mensaje> obtenerCapsulas() {
 //
 //    }
-//
+
+    //3.2 Consultar cápsula informativa
 //    @Transactional(readOnly = true)
-//    public ResponseEntity<Mensaje> salida() {
+//    public ResponseEntity<Mensaje> obtenerCapsula() {
+//
+//    }
+
+    //3.3 Registrar cápsula informativa
+//    @Transactional(rollbackFor = {SQLException.class})
+//    public ResponseEntity<Mensaje> registrarCapsula() {
+//
+//    }
+
+    //3.4 Consultar cápsulas informativas realizadas
+//    @Transactional(rollbackFor = {SQLException.class})
+//    public ResponseEntity<Mensaje> obtenerCapsulasRealizadas() {
+//
+//    }
+//    @Transactional(rollbackFor = {SQLException.class})
+//    public ResponseEntity<Mensaje> obtenerCapsulasRealizadasAdministrador() {
+//
+//    }
+
+    // 3.5 Modificar cápsula informativa
+//    @Transactional(rollbackFor = {SQLException.class})
+//    public ResponseEntity<Mensaje> modificarCapsula() {
+//
+//    }
+//    @Transactional(rollbackFor = {SQLException.class})
+//    public ResponseEntity<Mensaje> modificarCapsulaAdministrador() {
+//
+//    }
+
+    //3.6 Eliminar cápsula informativa
+//    @Transactional(rollbackFor = {SQLException.class})
+//    public ResponseEntity<Mensaje> eliminarCapsula() {
+//
+//    }
+//    @Transactional(rollbackFor = {SQLException.class})
+//    public ResponseEntity<Mensaje> eliminarCapsulaAdministrador() {
 //
 //    }
 }
