2a0ac222e3d36b265daab62ebbcfec68742d2fdd
20203tn077 <20203tn077@utez.edu.mx>
2022-04-18 02:08:51 -0500

    Desuscribirse

---
+++ src/main/java/mx/edu/utez/SIRIACI_servicio/controller/NotificacionesYMensajes/NotificacionesController.java
@@ -0,0 +1,43 @@
+package mx.edu.utez.SIRIACI_servicio.controller.NotificacionesYMensajes;
+
+import mx.edu.utez.SIRIACI_servicio.controller.ImagenDTO;
+import mx.edu.utez.SIRIACI_servicio.controller.Incidencias.IncidenciaController;
+import mx.edu.utez.SIRIACI_servicio.controller.Incidencias.IncidenciaDTO;
+import mx.edu.utez.SIRIACI_servicio.model.aspecto.Aspecto;
+import mx.edu.utez.SIRIACI_servicio.model.imagenIncidencia.ImagenIncidencia;
+import mx.edu.utez.SIRIACI_servicio.model.importancia.Importancia;
+import mx.edu.utez.SIRIACI_servicio.model.incidencia.Incidencia;
+import mx.edu.utez.SIRIACI_servicio.model.usuario.Usuario;
+import mx.edu.utez.SIRIACI_servicio.security.DetalleUsuario;
+import mx.edu.utez.SIRIACI_servicio.util.Mensaje;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/notificaciones")
+@CrossOrigin(origins = {"*"})
+public class NotificacionesController {
+    private final static Logger logger = LoggerFactory.getLogger(NotificacionesController.class);
+    @Autowired
+    NotificacionesService service;
+
+    @DeleteMapping("/")
+    public void desuscribirse(@RequestBody NotificacionesDTO notificacionesDTO) {
+        try {
+            service.desuscribirse(notificacionesDTO.getToken());
+        } catch (Exception e) {
+            logger.error("Error en método desuscribirse: " + e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/mx/edu/utez/SIRIACI_servicio/controller/NotificacionesYMensajes/NotificacionesDTO.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.SIRIACI_servicio.controller.NotificacionesYMensajes;
+
+public class NotificacionesDTO {
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
